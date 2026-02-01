a39863f20cd38cbd09ee60b8690a68a568e68953
Michael Weghorn <m.weghorn@posteo.de>
2026-02-01 01:04:09 +0100

    tdf#130857 qt weld: Support comment dialog
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Writer
    * enable "Edit" -> "Track Changes" -> "Record"
    * type something in the document
    * "Edit" -> "Track Changes" -> "Manage"
    * right-click on the entry in the treeview and select
      the "Edit Comment..." context menu entry
    
    Change-Id: Idf898b8d0ae16515a5b29aa15f985d02ddc5ba5f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198476
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -64,6 +64,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"cui/ui/additionsdialog.ui",
     u"cui/ui/breaknumberoption.ui",
     u"cui/ui/certdialog.ui",
+    u"cui/ui/comment.ui",
     u"cui/ui/editdictionarydialog.ui",
     u"cui/ui/fontfeaturesdialog.ui",
     u"cui/ui/fontfragment.ui",
