38ad366d6970d98c30b2d91158b1a2ef0cdcd010
Noel Grandin <noel.grandin@collabora.co.uk>
2026-02-02 12:54:24 +0200

    loplugin:unusedmethods
    Change-Id: Ic00fb2aceef3156c3cff442908374daac3861e7f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198535
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- include/editeng/outliner.hxx
+++ include/editeng/outliner.hxx
@@ -189,10 +189,6 @@ private:
         return maBullet.maText;
     }
 
-    BulletInfo const& GetBullet()
-    {
-        return maBullet;
-    }
                         Paragraph( sal_Int16 nDepth );
                         Paragraph( const Paragraph& ) = delete;
                         Paragraph( const ParagraphData& );
