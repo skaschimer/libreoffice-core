371078d1ad79bc3e52444fe4c8e2bcffc609d68b
Michael Weghorn <m.weghorn@posteo.de>
2026-02-02 17:02:08 +0100

    tdf#130857 qt weld: Support "Manage Conditional Formatting" dlg
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Calc
    * "Format" -> "Conditional" -> "Manage..."
    
    Change-Id: Id811f6c77f804096fb9e6a7032db0be36fb426ea
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198563
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -130,6 +130,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/BasicIDE/ui/gotolinedialog.ui",
     u"modules/BasicIDE/ui/newlibdialog.ui",
     u"modules/scalc/ui/colwidthdialog.ui",
+    u"modules/scalc/ui/condformatmanager.ui",
     u"modules/scalc/ui/conditionalformatdialog.ui",
     u"modules/scalc/ui/definedatabaserangedialog.ui",
     u"modules/scalc/ui/deletecells.ui",
