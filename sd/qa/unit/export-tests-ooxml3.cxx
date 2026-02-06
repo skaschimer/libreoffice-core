LibreOffice/core

commit 00d1c2800c10d542673ab6f5a5baa26aa1cbb20e

oox: deterministic oleObject stream numbering
... like in sw.

Change-Id: I4ba5d404c4160e907330832b7710c1d96430badf

include/oox/export/shapes.hxx
@@ -69,7 +69,7 @@ public:
 class OOX_DLLPUBLIC ShapeExport : public DrawingML {
 
 private:
-    static int mnEmbeddeDocumentCounter;
+    int m_nEmbeddedObjects;
     struct ShapeCheck
     {
         bool operator()( const css::uno::Reference< css::drawing::XShape>& s1, const css::uno::Reference< css::drawing::XShape>& s2 ) const

oox/source/export/shapes.cxx
@@ -359,11 +359,9 @@ bool URLTransformer::isExternalURL(const OUString& /*rURL*/) const
     if ( GETA(propName) ) \
         mAny >>= variable;
 
-// not thread safe
-int ShapeExport::mnEmbeddeDocumentCounter = 1;
-
 ShapeExport::ShapeExport( sal_Int32 nXmlNamespace, FSHelperPtr pFS, ShapeHashMap* pShapeMap, XmlFilterBase* pFB, DocumentType eDocumentType, DMLTextExport* pTextExport )
     : DrawingML( pFS, pFB, eDocumentType, pTextExport )
+    , m_nEmbeddedObjects(0)
     , mnShapeIdMax( 1 )
     , mnPictureIdMax( 1 )
     , mnXmlNamespace( nXmlNamespace )
@@ -1683,7 +1681,7 @@ ShapeExport& ShapeExport::WriteOLE2Shape( Reference< XShape > xShape )
     assert(!sRelationType.isEmpty());
     assert(!sSuffix.isEmpty());
 
-    OUString sFileName = "embeddings/oleObject" + OUString::number(mnEmbeddeDocumentCounter++) + "." + sSuffix;
+    OUString sFileName = "embeddings/oleObject" + OUString::number(++m_nEmbeddedObjects) + "." + sSuffix;
     uno::Reference<io::XOutputStream> const xOutStream(
         mpFB->openFragmentStream(
             OUString::createFromAscii(GetComponentDir()) + "/" + sFileName,

skaschimer/libreoffice-core

commit c917b55a8158bbaeac161ad1b806d5fac9811c46

tdf#170241 tdf#169952 sd: add unit test for embedded OLE obj export
Make sure all oleObjectN.bin files are exported.

Regression introduced in 00d1c2800c10d542673ab6f5a5baa26aa1cbb20e
fixed in 1c1120076a103f694e8430d4c85a2863e02ce959.

Change-Id: I9671a9b209ed9e4bfcb6b0eb73dafe6525537e2e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198841
Tested-by: Jenkins
Reviewed-by: Aron Budea <aron.budea@collabora.com>

sd/qa/unit/export-tests-ooxml3.cxx
@@ -1186,6 +1186,21 @@ CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest3, testTdf169952)
     CPPUNIT_ASSERT_MESSAGE("Without the fix chart2.xml is not exported", pXmlDoc);
 }
 
+CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest3, testTdf169952_multiple_OLEs)
+{
+    createSdImpressDoc("odp/tdf169952_multiple_OLEs.odp");
+    save(TestFilter::PPTX);
+
+    // Without the fix two of the three OLE objects would get lost, and only oleObject1.bin
+    // would exist
+    uno::Reference<packages::zip::XZipFileAccess2> xNameAccess
+        = packages::zip::ZipFileAccess::createWithURL(comphelper::getComponentContext(m_xSFactory),
+                                                      maTempFile.GetURL());
+    CPPUNIT_ASSERT_EQUAL(true, bool(xNameAccess->hasByName(u"ppt/embeddings/oleObject1.bin"_ustr)));
+    CPPUNIT_ASSERT_EQUAL(true, bool(xNameAccess->hasByName(u"ppt/embeddings/oleObject2.bin"_ustr)));
+    CPPUNIT_ASSERT_EQUAL(true, bool(xNameAccess->hasByName(u"ppt/embeddings/oleObject3.bin"_ustr)));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */