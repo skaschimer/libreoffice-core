LibreOffice/core

commit 6fe35fdbd39cee17cb1f4fc9494520e2bdcabcba

tdf#167128: SwRedlineAcceptDlg: move enabling controls after selection
It was done in InitAuthors, which was called before lcl_reselect;
and therefore, opening the dialog would not see the selection, and
not activate buttons properly.

Now the "Accept" and "Reject" are active upon dialog opening, when
there is a selection.

This required extracting the respective code to a separate function.

Change-Id: I3a02384b402312613fef4b2d3932d19c9b1acc19
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/186752
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sw/source/uibase/misc/redlndlg.cxx
@@ -310,6 +310,7 @@ void SwRedlineAcceptDlg::Init(SwRedlineTable::size_type nStart)
     InitAuthors();
 
     lcl_reselect(rTreeView, pSelectedEntryRedlineData);
+    EnableControls(pView);
 }
 
 static bool isAcceptRejectCommandsEnabled(const SwView& rView)
@@ -340,7 +341,6 @@ void SwRedlineAcceptDlg::InitAuthors()
     SwRedlineTable::size_type nCount = pSh ? pSh->GetRedlineCount() : 0;
 
     m_bOnlyFormatedRedlines = true;
-    bool bIsNotFormated = false;
 
     // determine authors
     for ( SwRedlineTable::size_type i = 0; i < nCount; i++)
@@ -366,6 +366,15 @@ void SwRedlineAcceptDlg::InitAuthors()
 
     if (pFilterPage->SelectAuthor(sOldAuthor) == -1 && !aStrings.empty())
         pFilterPage->SelectAuthor(aStrings[0]);
+}
+
+void SwRedlineAcceptDlg::EnableControls(const SwView* pView)
+{
+    if (!pView)
+        return;
+    SwWrtShell* pSh = pView->GetWrtShellPtr();
+    if (!pSh)
+        return;
 
     weld::TreeView& rTreeView = m_pTable->GetWidget();
     bool const bEnable = isAcceptRejectCommandsEnabled(*pView)
@@ -373,6 +382,7 @@ void SwRedlineAcceptDlg::InitAuthors()
         && !pSh->getIDocumentRedlineAccess().GetRedlinePassword().hasElements();
     bool bSel = rTreeView.get_selected(nullptr);
 
+    bool bIsNotFormated = false;
     rTreeView.selected_foreach([this, pSh, &bIsNotFormated](weld::TreeIter& rEntry){
         // find the selected redline
         // (fdo#57874: ignore, if the redline is already gone)
@@ -473,7 +483,7 @@ void SwRedlineAcceptDlg::Activate()
         m_pTPView->EnableAcceptAll(false);
         m_pTPView->EnableRejectAll(false);
         m_pTPView->EnableClearFormatAll(false);
-        // note: enabling is done in InitAuthors below
+        // note: enabling is done in EnableControls below
     }
 
     m_aUsedSeqNo.clear();
@@ -585,6 +595,7 @@ void SwRedlineAcceptDlg::Activate()
     InitAuthors();
 
     lcl_reselect(rTreeView, pSelectedEntryRedlineData);
+    EnableControls(pView);
 }
 
 void SwRedlineAcceptDlg::Notify(SfxBroadcaster& /*rBC*/, const SfxHint& rHint)
@@ -1315,9 +1326,6 @@ IMPL_LINK_NOARG(SwRedlineAcceptDlg, GotoHdl, Timer *, void)
     if (!pSh)
         return;
 
-    bool bIsNotFormated = false;
-    bool bSel = false;
-
     //#98883# don't select redlines while the dialog is not focused
     //#107938# But not only ask pTable if it has the focus. To move
     //         the selection to the selected redline any child of pParentDlg
@@ -1332,7 +1340,7 @@ IMPL_LINK_NOARG(SwRedlineAcceptDlg, GotoHdl, Timer *, void)
             pSh->EnterStdMode();
             SwViewShell::SetCareDialog(m_xParentDlg);
 
-            rTreeView.selected_foreach([this, pSh, &rTreeView, &xActEntry, &bIsNotFormated, &bSel](weld::TreeIter& rEntry){
+            rTreeView.selected_foreach([this, pSh, &rTreeView, &xActEntry](weld::TreeIter& rEntry){
                 rTreeView.copy_iterator(rEntry, *xActEntry);
                 if (rTreeView.get_iter_depth(rEntry))
                 {
@@ -1340,17 +1348,11 @@ IMPL_LINK_NOARG(SwRedlineAcceptDlg, GotoHdl, Timer *, void)
                     if (rTreeView.is_selected(*xActEntry))
                         return false;   // don't select twice
                 }
-                else
-                    bSel = true;
 
                 // #98864# find the selected redline (ignore, if the redline is already gone)
                 SwRedlineTable::size_type nPos = GetRedlinePos(*xActEntry);
                 if (nPos != SwRedlineTable::npos)
                 {
-
-                    const SwRangeRedline& rRedln = pSh->GetRedline( nPos );
-                    bIsNotFormated |= RedlineType::Format != rRedln.GetType();
-
                     if (pSh->GotoRedline(nPos, true))
                     {
                         pSh->SetInSelect();
@@ -1371,9 +1373,6 @@ IMPL_LINK_NOARG(SwRedlineAcceptDlg, GotoHdl, Timer *, void)
                             nPos = GetRedlinePos(*xChild);
                             if (nPos != SwRedlineTable::npos)
                             {
-                                const SwRangeRedline& rRedln = pSh->GetRedline( nPos );
-                                bIsNotFormated |= RedlineType::Format != rRedln.GetType();
-
                                 if (pSh->GotoRedline(nPos, true))
                                 {
                                     pSh->SetInSelect();
@@ -1393,14 +1392,7 @@ IMPL_LINK_NOARG(SwRedlineAcceptDlg, GotoHdl, Timer *, void)
         }
     }
 
-    bool const bEnable = isAcceptRejectCommandsEnabled(*pView)
-        && !pSh->getIDocumentRedlineAccess().GetRedlinePassword().hasElements();
-    m_pTPView->EnableAccept( bEnable && bSel /*&& !bReadonlySel*/ );
-    m_pTPView->EnableReject( bEnable && bSel /*&& !bReadonlySel*/ );
-    m_pTPView->EnableClearFormat( bEnable && bSel && !bIsNotFormated /*&& !bReadonlySel*/ );
-    m_pTPView->EnableAcceptAll( bEnable );
-    m_pTPView->EnableRejectAll( bEnable );
-    m_pTPView->EnableClearFormatAll( bEnable && m_bOnlyFormatedRedlines );
+    EnableControls(pView);
 }
 
 IMPL_LINK(SwRedlineAcceptDlg, CommandHdl, const CommandEvent&, rCEvt, bool)

sw/source/uibase/inc/redlndlg.hxx
@@ -50,6 +50,7 @@ struct SwRedlineDataParent
 };
 
 class SwRedlineDataParentSortArr : public o3tl::sorted_vector<SwRedlineDataParent*, o3tl::less_ptr_to > {};
+class SwView;
 
 class SW_DLLPUBLIC SwRedlineAcceptDlg final : public SfxListener
 {
@@ -101,6 +102,7 @@ class SW_DLLPUBLIC SwRedlineAcceptDlg final : public SfxListener
     SAL_DLLPRIVATE void          InsertParents(SwRedlineTable::size_type nStart, SwRedlineTable::size_type nEnd = SwRedlineTable::npos);
     SAL_DLLPRIVATE void          RemoveParents(SwRedlineTable::size_type nStart, SwRedlineTable::size_type nEnd);
     SAL_DLLPRIVATE void          InitAuthors();
+    SAL_DLLPRIVATE void          EnableControls(const SwView* pView);
 
     SAL_DLLPRIVATE static const OUString & GetActionImage(const SwRangeRedline& rRedln, sal_uInt16 nStack = 0,
                                                   bool bTableChanges = false, bool bRowChanges = false);

skaschimer/libreoffice-core

commit 6f672373ecb30565261395bb6ebed334c0649b7f

tdf#170701: only enable [Accept]/[Reject] for top-level changes
Regression after commit 6fe35fdbd39cee17cb1f4fc9494520e2bdcabcba
(tdf#167128: SwRedlineAcceptDlg: move enabling controls after
selection, 2025-06-20). The previous code, that enabled these
controls in GotoHdl, only set bSel to true for top-level items.

This change implements the same logic in EnableControls.

Change-Id: I70414ece801312d1b5e873db0440c0e28b76ef02
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199019
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins

sw/source/uibase/misc/redlndlg.cxx
@@ -379,10 +379,14 @@ void SwRedlineAcceptDlg::EnableControls(const SwView* pView)
     bool const bEnable = isAcceptRejectCommandsEnabled(*pView)
         && rTreeView.n_children() != 0
         && !pSh->getIDocumentRedlineAccess().GetRedlinePassword().hasElements();
-    std::unique_ptr<weld::TreeIter> pSelectedEntry = rTreeView.get_selected();
 
+    bool bAcceptReject = false;
     bool bIsNotFormated = false;
-    rTreeView.selected_foreach([this, pSh, &bIsNotFormated](weld::TreeIter& rEntry){
+    rTreeView.selected_foreach([this, pSh, &bIsNotFormated, &bAcceptReject](weld::TreeIter& rEntry){
+        // Only enable these controls for top-level redlines
+        if (m_pTable->GetWidget().get_iter_depth(rEntry) == 0)
+            bAcceptReject = true;
+
         // find the selected redline
         // (fdo#57874: ignore, if the redline is already gone)
         SwRedlineTable::size_type nPos = GetRedlinePos(rEntry);
@@ -395,9 +399,9 @@ void SwRedlineAcceptDlg::EnableControls(const SwView* pView)
         return false;
     });
 
-    m_rTPView.EnableAccept(bEnable && pSelectedEntry);
-    m_rTPView.EnableReject(bEnable && pSelectedEntry);
-    m_rTPView.EnableClearFormat(bEnable && !bIsNotFormated && pSelectedEntry);
+    m_rTPView.EnableAccept(bEnable && bAcceptReject);
+    m_rTPView.EnableReject(bEnable && bAcceptReject);
+    m_rTPView.EnableClearFormat(bEnable && !bIsNotFormated && bAcceptReject);
     m_rTPView.EnableAcceptAll(bEnable);
     m_rTPView.EnableRejectAll(bEnable);
     m_rTPView.EnableClearFormatAll(bEnable && m_bOnlyFormatedRedlines);