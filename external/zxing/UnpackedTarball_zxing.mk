e7f1f44afa1fcb4a84837ea07bdf5ff30c20330d
Xisco Fauli <xiscofauli@libreoffice.org>
2026-02-11 14:20:32 +0100

    Revert "wsl-as-helper: fix extracting zxing tarball (dangling symlinks)"
    This reverts commit 04585baf973742b0a400b53209391a99ae959d99.
    
    As discussed in https://github.com/zxing-cpp/zxing-cpp/issues/887
    this issue has been fixed after upgrading to 3.0.0 in
    commit 3b43b3164f5edce7ab69825639cd5b0fb300daa3
    Author: Xisco Fauli <xiscofauli@libreoffice.org>
    Date:   Wed Feb 11 14:04:41 2026 +0100
    
        zxing-cpp: Upgrade to 3.0.0
    
    The problem is also mentioned in https://github.com/zxing-cpp/zxing-cpp/releases/tag/v3.0.0
    
    Change-Id: Ie89ad444d3d304f5915621c23eec6cc46906bdb1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199179
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- external/zxing/UnpackedTarball_zxing.mk
+++ external/zxing/UnpackedTarball_zxing.mk
@@ -11,14 +11,6 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,zxing))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,zxing,$(ZXING_TARBALL)))
 
-ifneq ($(MSYSTEM),)
-# the 2.3.0 tarball contains dangling symlinks (to a submodule component/experimental backend)
-# git-bash/msys tar fails when extracting since MSYS defaults to not create those
-ifeq ($(filter winsymlinks%,$(MSYS)),)
-$(call gb_UnpackedTarball_get_target,zxing): export MSYS:=$(MSYS) winsymlinks
-endif
-endif
-
 $(eval $(call gb_UnpackedTarball_add_patches,zxing, \
 	external/zxing/0001-add-ZXVersion-h.patch \
 	external/zxing/gcc12-format.patch \
