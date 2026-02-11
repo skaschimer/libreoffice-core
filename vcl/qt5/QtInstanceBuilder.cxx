d0d334dc53003c266d67cfd57f51a3f9ca0c06d8
Michael Weghorn <m.weghorn@posteo.de>
2026-02-11 12:11:35 +0100

    tdf#130857 qt weld: Support Column Arrangement dialog
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Writer
    * "Tools" -> Bibliography Database"
    * "Data" -> "Column Arrangement"
    
    Change-Id: Ic0dbc7ea0aa2939d967b09818a97be507fa067bf
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199158
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -138,6 +138,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/BasicIDE/ui/gotolinedialog.ui",
     u"modules/BasicIDE/ui/newlibdialog.ui",
     u"modules/sbibliography/ui/choosedatasourcedialog.ui",
+    u"modules/sbibliography/ui/mappingdialog.ui",
     u"modules/scalc/ui/colwidthdialog.ui",
     u"modules/scalc/ui/condformatmanager.ui",
     u"modules/scalc/ui/conditionalformatdialog.ui",
