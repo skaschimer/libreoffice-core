b9487ea0403872fb1e6e33550afc5c9b47bd196a
Michael Weghorn <m.weghorn@posteo.de>
2026-02-13 10:15:12 +0100

    vcl: Simplify Menu::GetHelpText a bit
    If `pData->aHelpText` is non-empty, it is always
    returned at the end. Return it early, which gets
    rid of the need to repeatedly check whether
    `pData->aHelpText` is empty.
    
    Change-Id: Ie3956caa5e51ec755a2603e75962a5ff24d187d2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199338
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/source/window/menu.cxx
+++ vcl/source/window/menu.cxx
@@ -1094,8 +1094,10 @@ OUString Menu::GetHelpText( sal_uInt16 nItemId ) const
     if (!pData)
         return OUString();
 
-    if ( pData->aHelpText.isEmpty() &&
-         (( !pData->aHelpId.isEmpty()  ) || ( !pData->aCommandStr.isEmpty() )))
+    if (!pData->aHelpText.isEmpty())
+        return pData->aHelpText;
+
+    if (!pData->aHelpId.isEmpty() || !pData->aCommandStr.isEmpty())
     {
         Help* pHelp = Application::GetHelp();
         if ( pHelp )
@@ -1108,9 +1110,7 @@ OUString Menu::GetHelpText( sal_uInt16 nItemId ) const
     }
 
     //Fallback to Menu::GetAccessibleDescription without reentry to GetHelpText()
-    if (pData->aHelpText.isEmpty())
-        return pData->aAccessibleDescription;
-    return pData->aHelpText;
+    return pData->aAccessibleDescription;
 }
 
 void Menu::SetTipHelpText( sal_uInt16 nItemId, const OUString& rStr )
