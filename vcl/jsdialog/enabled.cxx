a19670c168c98770b14e9cb5c56056aa05205057
Andrea Gelmini <andrea.gelmini@gelma.net>
2026-02-15 16:24:16 +0100

    Fix typo
    Change-Id: Ia1ca86b18e3f6ca595c73f61b145a032e6fd3c1d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199410
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- vcl/jsdialog/enabled.cxx
+++ vcl/jsdialog/enabled.cxx
@@ -766,7 +766,7 @@ std::vector<OUString> completeCommonDialogList(const o3tl::sorted_vector<OUStrin
             // work for me
             else if (entry == u"uui/ui/macrowarnmedium.ui")
                 continue;
-            // This, for the forumula dialog, are really only available in calc
+            // This, for the formula dialog, are really only available in calc
             else if (entry == u"formula/ui/formuladialog.ui" ||
                      entry == u"formula/ui/functionpage.ui" ||
                      entry == u"formula/ui/parameter.ui" ||
