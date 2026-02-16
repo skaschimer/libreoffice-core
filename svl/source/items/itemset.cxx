LibreOffice/core

commit b1df5786458223f21f0eaa9627c0f4c6fa5f1747

tdf#166684 use hashing in CellAttributeHelper::registerAndCheck
reduces load time from 1m11s to 33s.

Change-Id: I911c101eb8692bf678e6f7147e64e503e9f2bf0a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196062
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
(cherry picked from commit 044885e896a441c96a6934b10cbb723fabcf262d)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196357
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

svl/source/items/itemset.cxx
@@ -27,6 +27,7 @@
 
 #include <libxml/xmlwriter.h>
 #include <tools/XmlWriter.hxx>
+#include <o3tl/hash_combine.hxx>
 
 #include <sal/log.hxx>
 #include <svl/itemset.hxx>
@@ -1287,6 +1288,24 @@ bool SfxItemSet::Equals(const SfxItemSet &rCmp, bool bComparePool) const
     }
 
     return true;
+}
+
+size_t SfxItemSet::GetHashCode() const
+{
+    size_t seed = 0;
+
+    for (PoolItemMap::const_iterator it(m_aPoolItemMap.begin()); it != m_aPoolItemMap.end(); it++)
+    {
+        const sal_uInt16 nWhich = it->first;
+        const SfxPoolItem *pItem = it->second;
+
+        o3tl::hash_combine(seed, nWhich);
+        if (!IsInvalidItem(pItem) && !IsDisabledItem(pItem)
+            && pItem && pItem->supportsHashCode())
+            o3tl::hash_combine(seed, pItem->hashCode());
+    }
+
+    return seed;
 }
 
 std::unique_ptr<SfxItemSet> SfxItemSet::Clone(bool bItems, SfxItemPool *pToPool ) const

include/svl/itemset.hxx
@@ -246,6 +246,7 @@ public:
     const SfxItemSet*           GetParent() const { return m_pParent; }
 
     bool                        operator==(const SfxItemSet &) const;
+    size_t                      GetHashCode() const;
 
     /** Compare possibly ignoring SfxItemPool pointer.
 

sc/inc/patattr.hxx
@@ -130,6 +130,7 @@ class SAL_DLLPUBLIC_RTTI ScPatternAttr final
     friend class CellAttributeHelper;
 
     SfxItemSet                  maLocalSfxItemSet;
+    mutable std::optional<size_t> moHashCode;
     std::optional<OUString>     moName;
     mutable std::optional<bool> mxVisible;
     mutable std::optional<sal_uInt32> mxNumberFormatKey;
@@ -149,6 +150,7 @@ public:
     SC_DLLPUBLIC ~ScPatternAttr();
 
     bool operator==(const ScPatternAttr& rCmp) const;
+    size_t GetHashCode() const { if (!moHashCode) CalcHashCode(); return *moHashCode; }
 
     // version that allows nullptrs
     SC_DLLPUBLIC static bool areSame(const ScPatternAttr* pItem1, const ScPatternAttr* pItem2);
@@ -285,6 +287,7 @@ private:
     LanguageType            GetLanguageType() const;
     void                    InvalidateCaches();
     void                    InvalidateCacheFor(sal_uInt16 nWhich);
+    void                    CalcHashCode() const;
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sc/source/core/data/patattr.cxx
@@ -115,12 +115,14 @@ const ScPatternAttr* CellAttributeHelper::registerAndCheck(const ScPatternAttr& 
     }
     const OUString* pCandidateStyleName = rCandidate.GetStyleName();
     auto it = maRegisteredCellAttributes.lower_bound(pCandidateStyleName);
+    const size_t nCandidateHashCode = rCandidate.GetHashCode();
     for (; it != maRegisteredCellAttributes.end(); ++it)
     {
         const ScPatternAttr* pCheck = *it;
         if (CompareStringPtr(pCheck->GetStyleName(), pCandidateStyleName) != 0)
             break;
-        if (ScPatternAttr::areSame(pCheck, &rCandidate))
+        if (nCandidateHashCode == pCheck->GetHashCode()
+            && ScPatternAttr::areSame(pCheck, &rCandidate))
         {
             pCheck->mnRefCount++;
             if (bPassingOwnership)
@@ -470,6 +472,15 @@ bool ScPatternAttr::operator==(const ScPatternAttr& rCmp) const
     // that hints to different WhichRanges, but WhichRanges are not compared in
     // the std-operator (but the Items - if needed - as was here done locally)
     return maLocalSfxItemSet == rCmp.maLocalSfxItemSet;
+}
+
+void ScPatternAttr::CalcHashCode() const
+{
+    size_t nHash = 0;
+    if (const OUString* pName = GetStyleName())
+        nHash = pName->hashCode();
+    o3tl::hash_combine(nHash, maLocalSfxItemSet.GetHashCode());
+    moHashCode = nHash;
 }
 
 bool ScPatternAttr::areSame(const ScPatternAttr* pItem1, const ScPatternAttr* pItem2)
@@ -1799,6 +1810,7 @@ void ScPatternAttr::InvalidateCaches()
     mxVisible.reset();
     mxNumberFormatKey.reset();
     mxLanguageType.reset();
+    moHashCode.reset();
 }
 
 SfxItemSet& ScPatternAttr::GetItemSetWritable()

skaschimer/libreoffice-core

commit 7d519f0706356c2b442143fe8f70f38f7e776b05

tdf#170827 FILEOPEN XLSX File hangs Calc when opened
regression from
  commit b1df5786458223f21f0eaa9627c0f4c6fa5f1747
  Author: Noel Grandin <noel.grandin@collabora.co.uk>
  Date:   Sun Dec 21 19:55:04 2025 +0200
  tdf#166684 use hashing in CellAttributeHelper::registerAndCheck

Change-Id: Ibe17b14fd93da584aff551e774150d42294055c6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199451
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svl/source/items/itemset.cxx
@@ -1292,6 +1292,12 @@ bool SfxItemSet::Equals(const SfxItemSet &rCmp, bool bComparePool) const
 
 size_t SfxItemSet::GetHashCode() const
 {
+    // We are calculating a hash in an odd way here. This is not ideal, because
+    // it does not produce as good a result as using o3tl::hash_combine.
+    // However, m_aPoolItemMap is an __unordered__ map, and so using o3tl::hash_combine
+    // will not necessarily produce the same hash for two otherwise identical SfxItemSet.
+    // Using addition is invariant with respect to ordering, so we sacrifice some hashing
+    // quality in favour of correctness.
     size_t seed = 0;
 
     for (PoolItemMap::const_iterator it(m_aPoolItemMap.begin()); it != m_aPoolItemMap.end(); it++)
@@ -1299,10 +1305,10 @@ size_t SfxItemSet::GetHashCode() const
         const sal_uInt16 nWhich = it->first;
         const SfxPoolItem *pItem = it->second;
 
-        o3tl::hash_combine(seed, nWhich);
+        seed += nWhich;
         if (!IsInvalidItem(pItem) && !IsDisabledItem(pItem)
             && pItem && pItem->supportsHashCode())
-            o3tl::hash_combine(seed, pItem->hashCode());
+            seed += pItem->hashCode();
     }
 
     return seed;