d43fe70f9f815476c11b84d550d34c16f6e7a3ca
Yoon6 <yoonda5898@gmail.com>
2026-05-02 21:48:09 +0900

    tdf#143148 toolkit: use #pragma once in diagnose_ex.hxx
    Change-Id: I721ef55002adf6af4db29e082b56d2eed81080a0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/204668
    Tested-by: Julien Nabet <serval2412@yahoo.fr>
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- include/comphelper/diagnose_ex.hxx
+++ include/comphelper/diagnose_ex.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_COMPHELPER_DIAGNOSE_EX_HXX
-#define INCLUDED_COMPHELPER_DIAGNOSE_EX_HXX
+
+#pragma once
 
 #include <osl/diagnose.h>
 #include <rtl/ustring.hxx>
@@ -174,8 +174,6 @@ COMPHELPER_DLLPUBLIC OString exceptionToString(css::uno::Any const & caughtEx);
     do { \
         SAL_INFO(area, stream); \
     } while (false)
-#endif
-
 #endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
