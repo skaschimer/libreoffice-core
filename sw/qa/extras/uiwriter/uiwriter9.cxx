LibreOffice/core

Autocorrect: undo of a typographic quote left a straight quote behind
When an apostrophe or quotation mark is typed, autocorrect replaces it
with the matching typographic quote. This was done in two edits: the
typed straight character was inserted first, then replaced with the
typographic one. Since each edit is its own undo step, undoing the
autocorrect first turned the quote back into a straight quote and only a
second undo removed it. In languages that never use straight quotes, for
example French, typing 'un and undoing left a stray straight quote in the
text.

Build the typographic quote with a single edit, so one undo removes the
whole quote.

The change is limited to the quote handling. Autocorrections that insert
a character and then act on something else, such as turning a web or mail
address into a link after a space, keep their existing per-step undo

Change-Id: Ibf47fb8f37d29d5ba180ed692f730d20e9f3e431
Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/5897
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

engine/sw/qa/extras/uiwriter/uiwriter9.cxx
@@ -1918,6 +1918,52 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testStyleApplyForwardsOriginalName)
                          getProperty<OUString>(getParagraph(1), u"ParaStyleName"_ustr));
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testAutocorrectSingleQuoteUndo)
+{
+    // Typing an apostrophe autocorrects it to a typographic one. Undoing the
+    // autocorrect must remove the whole quote in a single step, not leave a
+    // straight quote behind.
+    createSwDoc();
+    SwWrtShell* pWrtShell = getSwDocShell()->GetWrtShell();
+    SwXTextDocument* pTextDoc = getSwTextDoc();
+
+    auto getText = [&]() -> OUString {
+        return pWrtShell->getShellCursor(false)->GetPoint()->GetNode().GetTextNode()->GetText();
+    };
+
+    pTextDoc->postKeyEvent(KIT_KEYEVENT_KEYINPUT, 'c', 0);
+    pTextDoc->postKeyEvent(KIT_KEYEVENT_KEYINPUT, 'e', 0);
+    pTextDoc->postKeyEvent(KIT_KEYEVENT_KEYINPUT, '\'', 0);
+    Scheduler::ProcessEventsToIdle();
+
+    // The straight apostrophe became a typographic one (U+2019).
+    CPPUNIT_ASSERT_EQUAL(u"ce\u2019"_ustr, getText());
+
+    pWrtShell->Undo();
+    Scheduler::ProcessEventsToIdle();
+
+    // One undo removes the quote entirely. Without the fix the quote turned
+    // back into a straight one (U+0027) and only a second undo removed it.
+    CPPUNIT_ASSERT_EQUAL(u"ce"_ustr, getText());
+
+    // The same holds when more is typed after the quote and it is undone: the
+    // undo that reaches the quote removes it rather than exposing a straight
+    // quote in between.
+    pTextDoc->postKeyEvent(KIT_KEYEVENT_KEYINPUT, '\'', 0);
+    pTextDoc->postKeyEvent(KIT_KEYEVENT_KEYINPUT, 'u', 0);
+    pTextDoc->postKeyEvent(KIT_KEYEVENT_KEYINPUT, 'n', 0);
+    Scheduler::ProcessEventsToIdle();
+    CPPUNIT_ASSERT_EQUAL(u"ce\u2019un"_ustr, getText());
+
+    pWrtShell->Undo();
+    Scheduler::ProcessEventsToIdle();
+    CPPUNIT_ASSERT_EQUAL(u"ce\u2019"_ustr, getText());
+
+    pWrtShell->Undo();
+    Scheduler::ProcessEventsToIdle();
+    CPPUNIT_ASSERT_EQUAL(u"ce"_ustr, getText());
+}
+
 } // end of anonymous namespace
 CPPUNIT_PLUGIN_IMPLEMENT();
 

skaschimer/libreoffice-core

(cherry picked from commit e2ba52e1996f81ff3b93fa70ecf625b7709d07fb)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/207621
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
Tested-by: Jenkins

sw/qa/extras/uiwriter/uiwriter9.cxx
@@ -1928,6 +1928,52 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testTdf59121_UndoRedoCutBookmark)
     CPPUNIT_ASSERT_EQUAL(u"foo"_ustr, (*(rIDMA.getAllMarksBegin()))->GetName().toString());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testAutocorrectSingleQuoteUndo)
+{
+    // Typing an apostrophe autocorrects it to a typographic one. Undoing the
+    // autocorrect must remove the whole quote in a single step, not leave a
+    // straight quote behind.
+    createSwDoc();
+    SwWrtShell* pWrtShell = getSwDocShell()->GetWrtShell();
+    SwXTextDocument* pTextDoc = getSwTextDoc();
+
+    auto getText = [&]() -> OUString {
+        return pWrtShell->getShellCursor(false)->GetPoint()->GetNode().GetTextNode()->GetText();
+    };
+
+    pTextDoc->postKeyEvent(LOK_KEYEVENT_KEYINPUT, 'c', 0);
+    pTextDoc->postKeyEvent(LOK_KEYEVENT_KEYINPUT, 'e', 0);
+    pTextDoc->postKeyEvent(LOK_KEYEVENT_KEYINPUT, '\'', 0);
+    Scheduler::ProcessEventsToIdle();
+
+    // The straight apostrophe became a typographic one (U+2019).
+    CPPUNIT_ASSERT_EQUAL(u"ce\u2019"_ustr, getText());
+
+    pWrtShell->Undo();
+    Scheduler::ProcessEventsToIdle();
+
+    // One undo removes the quote entirely. Without the fix the quote turned
+    // back into a straight one (U+0027) and only a second undo removed it.
+    CPPUNIT_ASSERT_EQUAL(u"ce"_ustr, getText());
+
+    // The same holds when more is typed after the quote and it is undone: the
+    // undo that reaches the quote removes it rather than exposing a straight
+    // quote in between.
+    pTextDoc->postKeyEvent(LOK_KEYEVENT_KEYINPUT, '\'', 0);
+    pTextDoc->postKeyEvent(LOK_KEYEVENT_KEYINPUT, 'u', 0);
+    pTextDoc->postKeyEvent(LOK_KEYEVENT_KEYINPUT, 'n', 0);
+    Scheduler::ProcessEventsToIdle();
+    CPPUNIT_ASSERT_EQUAL(u"ce\u2019un"_ustr, getText());
+
+    pWrtShell->Undo();
+    Scheduler::ProcessEventsToIdle();
+    CPPUNIT_ASSERT_EQUAL(u"ce\u2019"_ustr, getText());
+
+    pWrtShell->Undo();
+    Scheduler::ProcessEventsToIdle();
+    CPPUNIT_ASSERT_EQUAL(u"ce"_ustr, getText());
+}
+
 } // end of anonymous namespace
 CPPUNIT_PLUGIN_IMPLEMENT();
 