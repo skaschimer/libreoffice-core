279746af7d200e62aa4c865e05e9c7eb1b04e0fb
Michael Weghorn <m.weghorn@posteo.de>
2026-07-06 09:58:09 +0200

    tdf#130857 qt weld: Support custom LO file dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Writer
    * in "Tools" -> "Options" -> "LibreOfficeDev"
      -> "General", enable "Use LibreOfficeDev open/save dialogs"
    * "File" -> "Save As..."
    
    Change-Id: I89c6cb5afad1196c54f9d1376c0aec9583323d56
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/207655
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -154,6 +154,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"filter/ui/xmlfiltertabpagetransformation.ui",
     u"filter/ui/xmlfiltersettings.ui",
     u"filter/ui/xsltfilterdialog.ui",
+    u"fps/ui/explorerfiledialog.ui",
     u"fps/ui/foldernamedialog.ui",
     u"modules/BasicIDE/ui/colorscheme.ui",
     u"modules/BasicIDE/ui/defaultlanguage.ui",
