3b4174fcc6ddb97a28b1b299ee316a99c615c020
Michael Weghorn <m.weghorn@posteo.de>
2026-07-06 12:09:08 +0200

    tdf#130857 qt weld: Support "Create Database Link" dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Writer
    * "Tools" -> "Options"
    * "LibreOfficeDev Base" -> "Databases"
    * press the "New..." button
    
    Change-Id: I019a6ab84cfb45568c74eb6a539dfb17e746dab4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/207694
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -71,6 +71,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"cui/ui/breaknumberoption.ui",
     u"cui/ui/certdialog.ui",
     u"cui/ui/comment.ui",
+    u"cui/ui/databaselinkdialog.ui",
     u"cui/ui/editdictionarydialog.ui",
     u"cui/ui/editmodulesdialog.ui",
     u"cui/ui/eventassigndialog.ui",
