994d81b724508209b5836b07ce4ce981d229b4bb
Michael Weghorn <m.weghorn@posteo.de>
2026-07-08 11:50:00 +0200

    tdf#130857 qt weld: Support "Proxy" options page
    Declare support for this options tab page conditional
    on the parent being a native widget.
    
    This means that native Qt widgets will be used once support
    for the "Tools" -> "Options" dialog is declared when
    using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    While more work will be needed for that dialog, this
    page newly declared as supported works fine in a quick
    test with the dialog's UI file ("cui/ui/optionsdialog.ui")
    also being declared as supported locally.
    
    Before that dialog can be enabled "for real", all remaining
    tab pages still need to be looked into. But then, all already
    supported tab pages will "just work", without having to be added then,
    so this is one step towards future support for the dialog.
    
    The tab page can be triggered like this:
    
    * start Writer
    * "Tools" -> "Options"
    * "Internet" -> "Proxy"
    
    Change-Id: I61c9c316e30f209017e26fc87239056773a8ed21
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/207766
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -434,6 +434,7 @@ constexpr auto SUPPORTED_WITH_QT_PARENT = frozen::make_unordered_set<std::u16str
     u"cui/ui/optlingupage.ui",
     u"cui/ui/optonlineupdatepage.ui",
     u"cui/ui/optpathspage.ui",
+    u"cui/ui/optproxypage.ui",
     u"cui/ui/optsavepage.ui",
     u"cui/ui/optsecuritypage.ui",
     u"cui/ui/optuserpage.ui",
