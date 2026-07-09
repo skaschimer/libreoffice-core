6e9e851e54c443feabf3d484e93d0a6bb250005a
Michael Weghorn <m.weghorn@posteo.de>
2026-07-09 10:18:14 +0200

    tdf#130857 qt weld: Support sc "Copy List" dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Calc
    * insert something into cells A1:B2
    * select cells A1:B2
    * "Tools" -> "Options..."
    * "LibreOfficeDev Calc" -> "Sort Lists"
    * press the "Copy" button
    
    Change-Id: Ib50af2bb6a6fef34e6e9a488af0da3554403185b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/207919
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -168,6 +168,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/sbibliography/ui/choosedatasourcedialog.ui",
     u"modules/sbibliography/ui/mappingdialog.ui",
     u"modules/scalc/ui/chardialog.ui",
+    u"modules/scalc/ui/colorrowdialog.ui",
     u"modules/scalc/ui/colwidthdialog.ui",
     u"modules/scalc/ui/condformatmanager.ui",
     u"modules/scalc/ui/conditionalformatdialog.ui",
