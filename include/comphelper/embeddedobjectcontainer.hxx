3ee90bca131b9752acc71f3bc04f033a970fe63a
ApurvR20 <apurvrajput@proton.me>
2026-07-13 18:26:43 +0530

    tdf#143148: Use pragma once instead of include guards
    Change-Id: If630c4e4c66cebb05751bd857ec948fc6f2a5a6c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208035
    Reviewed-by: Roman Kuznetsov <antilibreoffice@gmail.com>
    Tested-by: Jenkins

--- include/comphelper/embeddedobjectcontainer.hxx
+++ include/comphelper/embeddedobjectcontainer.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_COMPHELPER_EMBEDDEDOBJECTCONTAINER_HXX
-#define INCLUDED_COMPHELPER_EMBEDDEDOBJECTCONTAINER_HXX
+
+#pragma once
 
 #include <comphelper/comphelperdllapi.h>
 
@@ -209,7 +209,5 @@ public:
 };
 
 }
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
