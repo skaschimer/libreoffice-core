1791090d7a557992a64cb8115e091e322ba8e951
Michael Weghorn <m.weghorn@posteo.de>
2026-07-14 14:34:06 +0200

    tdf#130857 qt weld: Support Calc "Regression" dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Calc
    * "Data" -> "Statistics" -> "Regression..."
    
    Change-Id: I6a6967994c223cee246d815afa799987f0b5072f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208098
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -207,6 +207,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/scalc/ui/pastespecial.ui",
     u"modules/scalc/ui/pivotfielddialog.ui",
     u"modules/scalc/ui/protectsheetdlg.ui",
+    u"modules/scalc/ui/regressiondialog.ui",
     u"modules/scalc/ui/rowheightdialog.ui",
     u"modules/scalc/ui/samplingdialog.ui",
     u"modules/scalc/ui/scenariodialog.ui",
