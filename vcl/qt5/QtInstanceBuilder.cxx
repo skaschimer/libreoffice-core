7080f51bf869d06616c4e3c47670a21c0a7cd12a
Michael Weghorn <m.weghorn@posteo.de>
2026-07-14 14:12:12 +0200

    tdf#130857 qt weld: Support Calc "Covariance" dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Calc
    * "Data" -> "Statistics" -> "Covariance..."
    
    Change-Id: I973973b8c436139b118df4ca14f773fa5a04ad6e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208092
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -176,6 +176,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/scalc/ui/condformatmanager.ui",
     u"modules/scalc/ui/conditionalformatdialog.ui",
     u"modules/scalc/ui/correlationdialog.ui",
+    u"modules/scalc/ui/covariancedialog.ui",
     u"modules/scalc/ui/databaroptions.ui",
     u"modules/scalc/ui/datafieldoptionsdialog.ui",
     u"modules/scalc/ui/definedatabaserangedialog.ui",
