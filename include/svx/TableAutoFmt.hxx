skaschimer/libreoffice-core

commit 4fb31ea85f94c0cffba2282a119811833e584570

tdf#105933 GSoC: New Dialog to Edit Table Styles
- Introduce 'SvxAutoFormat' which combines the common funtionality of
Writer and Calc Autoformats.
- Replace module specific 'AutoFormat' dialog and preview with shared
dialog and preview in 'svx'.
- tdf#49437: Load and Save Autoformats through XML instead of
'autotbl.fmt'.
- New Dialog to edit and create table styles shared b/w Writer and Calc.

Change-Id: I7fa108e527141c05703e0e14298eea818fd10378
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190024
Tested-by: Heiko Tietze <heiko.tietze@documentfoundation.org>
Tested-by: Jenkins
Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

include/svx/TableAutoFmt.hxx
@@ -1,0 +1,209 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#pragma once
+
+#include <svl/itemprop.hxx>
+#include <i18nlangtag/lang.h>
+#include <svx/autoformathelper.hxx>
+#include <bitset>
+
+namespace Autoformat
+{
+enum PropertyIndex : sal_uInt8
+{
+    PROP_FONT = 0,
+    PROP_HEIGHT,
+    PROP_WEIGHT,
+    PROP_POSTURE,
+    PROP_CJK_FONT,
+    PROP_CJK_HEIGHT,
+    PROP_CJK_WEIGHT,
+    PROP_CJK_POSTURE,
+    PROP_CTL_FONT,
+    PROP_CTL_HEIGHT,
+    PROP_CTL_WEIGHT,
+    PROP_CTL_POSTURE,
+    PROP_UNDERLINE,
+    PROP_COLOR,
+    PROP_BOX,
+    PROP_BACKGROUND,
+    PROP_HOR_JUSTIFY,
+    PROP_VER_JUSTIFY,
+    PROP_PADDING,
+    PROP_COUNT
+};
+
+enum Elements : sal_uInt8
+{
+    FIRST_ROW = 0,
+    LAST_ROW,
+    FIRST_COL,
+    LAST_COL,
+    BODY,
+    EVEN_ROW,
+    ODD_ROW,
+    EVEN_COL,
+    ODD_COL,
+    BACKGROUND,
+    FIRST_ROW_EVEN_COL,
+    LAST_ROW_EVEN_COL,
+    FIRST_ROW_END_COL,
+    FIRST_ROW_START_COL,
+    LAST_ROW_END_COL,
+    LAST_ROW_START_COL,
+    ELEMENT_COUNT
+};
+}
+
+using namespace Autoformat;
+
+class SVX_DLLPUBLIC SvxAutoFormatDataField : public AutoFormatBase
+{
+private:
+    // number format
+    rtl::OUString maNumFormatString;
+    LanguageType meSysLanguage;
+    LanguageType meNumFormatLanguage;
+
+    std::bitset<Autoformat::PROP_COUNT> mSetProperties;
+
+public:
+    SvxAutoFormatDataField(){};
+    SvxAutoFormatDataField(const SvxAutoFormatDataField& rCopy);
+
+    void GetValueFormat(OUString& rFormat, LanguageType& rLng, LanguageType& rSys) const;
+    const OUString& GetNumFormatString() const noexcept { return maNumFormatString; }
+    const LanguageType& GetSysLanguage() const noexcept { return meSysLanguage; }
+    const LanguageType& GetNumFormatLanguage() const noexcept { return meNumFormatLanguage; }
+
+    void SetValueFormat(const OUString& rFormat, LanguageType eLng, LanguageType eSys);
+    void SetNumFormatString(const OUString& rNew) { maNumFormatString = rNew; }
+    void SetSysLanguage(const LanguageType& rNew) noexcept { meSysLanguage = rNew; }
+    void SetNumFormatLanguage(const LanguageType& rNew) noexcept { meNumFormatLanguage = rNew; }
+
+    bool IsPropertySet(PropertyIndex prop) const noexcept
+    {
+        return prop < PROP_COUNT && mSetProperties[prop];
+    }
+    void SetPropertyFlag(PropertyIndex prop, bool set = true) noexcept
+    {
+        if (prop < PROP_COUNT)
+            mSetProperties[prop] = set;
+    }
+    std::bitset<PROP_COUNT> GetPropertySet() const { return mSetProperties; }
+    void SetPropertySet(std::bitset<PROP_COUNT> aSet) { mSetProperties = aSet; }
+};
+
+class SVX_DLLPUBLIC SvxAutoFormatData
+{
+private:
+    OUString maName;
+
+    // Common flags of Calc and Writer.
+    bool mbInclFont : 1;
+    bool mbInclJustify : 1;
+    bool mbInclFrame : 1;
+    bool mbInclBackground : 1;
+    // Calc specific flags
+    bool mbInclValueFormat : 1;
+    bool mbInclWidthHeight : 1;
+
+    bool mbUseFirstRowStyles : 1;
+    bool mbUseLastRowStyles : 1;
+    bool mbUseFirstColStyles : 1;
+    bool mbUseLastColStyles : 1;
+    bool mbUseBandedRowStyles : 1;
+    bool mbUseBandedColStyles : 1;
+
+    OUString maParent;
+
+public:
+    SvxAutoFormatData();
+    SvxAutoFormatData(const SvxAutoFormatData& rData);
+    virtual ~SvxAutoFormatData() = default;
+
+    bool IsFont() const noexcept { return mbInclFont; }
+    bool IsJustify() const noexcept { return mbInclJustify; }
+    bool IsFrame() const noexcept { return mbInclFrame; }
+    bool IsBackground() const noexcept { return mbInclBackground; }
+    bool IsValueFormat() const noexcept { return mbInclValueFormat; }
+    bool IsWidthHeight() const noexcept { return mbInclWidthHeight; }
+
+    bool UseFirstRowStyles() const noexcept { return mbUseFirstRowStyles; }
+    bool UseLastRowStyles() const noexcept { return mbUseLastRowStyles; }
+    bool UseFirstColStyles() const noexcept { return mbUseFirstColStyles; }
+    bool UseLastColStyles() const noexcept { return mbUseLastColStyles; }
+    bool UseBandedRowStyles() const noexcept { return mbUseBandedRowStyles; }
+    bool UseBandedColStyles() const noexcept { return mbUseBandedColStyles; }
+
+    void SetFont(const bool bNew) noexcept { mbInclFont = bNew; }
+    void SetJustify(const bool bNew) noexcept { mbInclJustify = bNew; }
+    void SetFrame(const bool bNew) noexcept { mbInclFrame = bNew; }
+    void SetBackground(const bool bNew) noexcept { mbInclBackground = bNew; }
+    void SetValueFormat(const bool bNew) noexcept { mbInclValueFormat = bNew; }
+    void SetWidthHeight(const bool bNew) noexcept { mbInclWidthHeight = bNew; }
+    void SetParent(const OUString& rParentName) { maParent = rParentName; }
+
+    void SetUseFirstRowStyles(const bool bNew) noexcept { mbUseFirstRowStyles = bNew; }
+    void SetUseFirstColStyles(const bool bNew) noexcept { mbUseFirstColStyles = bNew; }
+    void SetUseLastRowStyles(const bool bNew) noexcept { mbUseLastRowStyles = bNew; }
+    void SetUseLastColStyles(const bool bNew) noexcept { mbUseLastColStyles = bNew; }
+    void SetUseBandedRowStyles(const bool bNew) noexcept { mbUseBandedRowStyles = bNew; }
+    void SetUseBandedColStyles(const bool bNew) noexcept { mbUseBandedColStyles = bNew; }
+
+    const OUString& GetName() const noexcept { return maName; }
+    const OUString& GetParent() const noexcept { return maParent; }
+
+    virtual void SetName(const OUString& rName) { maName = rName; }
+    virtual SvxAutoFormatDataField* GetField(size_t nIndex) = 0;
+    virtual const SvxAutoFormatDataField* GetField(size_t nIndex) const = 0;
+    virtual SvxAutoFormatDataField* GetDefaultField() const = 0;
+    virtual bool SetField(size_t nIndex, const SvxAutoFormatDataField& aField) = 0;
+    virtual SvxAutoFormatData* MakeCopy() const = 0;
+
+    void FillToItemSet(size_t nIndex, SfxItemSet& rItemSet) const;
+    void MergeStyle(const SvxAutoFormatData& pData);
+    void ResetAutoFormat(SvxAutoFormatData& pOld);
+
+    SvxAutoFormatData& operator=(const SvxAutoFormatData& rCopy);
+};
+
+class SVX_DLLPUBLIC SvxAutoFormat
+{
+public:
+    virtual ~SvxAutoFormat();
+
+    virtual const SvxAutoFormatData* GetData(size_t nIndex) const = 0;
+    virtual SvxAutoFormatData* GetData(size_t nIndex) = 0;
+    virtual bool InsertAutoFormat(SvxAutoFormatData* pFormat) = 0;
+    virtual SvxAutoFormatData* ReleaseAutoFormat(const OUString& rName) = 0;
+    virtual SvxAutoFormatData* FindAutoFormat(const OUString& rName) const = 0;
+    virtual SvxAutoFormatData* GetDefaultData() = 0;
+    virtual SvxAutoFormatDataField* GetDefaultField() = 0;
+    virtual const SvxAutoFormatData* GetResolvedStyle(const SvxAutoFormatData* pData) const = 0;
+    virtual size_t size() const = 0;
+    virtual void ResetParent(const OUString& rName);
+
+    static const SfxItemPropertySet& GetTablePropertySet();
+
+    bool Load(bool bWriter);
+    bool Save();
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/svx/strings.hrc
@@ -1883,6 +1883,22 @@
 #define RID_SVXSTR_FOOTER                           NC_("RID_SVXSTR_FOOTER", "Footer")
 #define RID_SVXSTR_HEADER                           NC_("RID_SVXSTR_HEADER", "Header")
 
+// strings related to table autoformat styles dialog
+#define RID_SVXSTR_ADD_AUTOFORMAT_TITLE                    NC_("RID_SVXSTR_ADD_AUTOFORMAT_TITLE", "Add AutoFormat" )
+#define RID_SVXSTR_RENAME_AUTOFORMAT_TITLE                 NC_("RID_SVXSTR_RENAME_AUTOFORMAT_TITLE", "Rename AutoFormat" )
+#define RID_SVXSTR_ADD_AUTOFORMAT_LABEL                    NC_("RID_SVXSTR_ADD_AUTOFORMAT_LABEL", "Name" )
+#define RID_SVXSTR_DEL_AUTOFORMAT_TITLE                    NC_("RID_SVXSTR_DEL_AUTOFORMAT_TITLE", "Delete AutoFormat" )
+#define RID_SVXSTR_DEL_AUTOFORMAT_MSG                      NC_("RID_SVXSTR_DEL_AUTOFORMAT_MSG", "Do you want to delete the following AutoFormat?" )
+#define RID_SVXSTR_BTN_AUTOFORMAT_CLOSE                    NC_("RID_SVXSTR_BTN_AUTOFORMAT_CLOSE", "~Close" )
+#define RID_SVXSTR_JAN                                     NC_("RID_SVXSTR_JAN", "Jan" )
+#define RID_SVXSTR_FEB                                     NC_("RID_SVXSTR_FEB", "Feb" )
+#define RID_SVXSTR_MAR                                     NC_("RID_SVXSTR_MAR", "Mar" )
+#define RID_SVXSTR_NORTH                                   NC_("RID_SVXSTR_NORTH", "North" )
+#define RID_SVXSTR_MID                                     NC_("RID_SVXSTR_MID", "Mid" )
+#define RID_SVXSTR_SOUTH                                   NC_("RID_SVXSTR_SOUTH", "South" )
+#define RID_SVXSTR_SUM                                     NC_("RID_SVXSTR_SUM", "Total" )
+#define RID_SVXSTR_INVALID_AUTOFORMAT_NAME                 NC_("RID_SVXSTR_INVALID_AUTOFORMAT_NAME", "You have entered an invalid name.\nThe desired AutoFormat could not be created. \nTry again using a different name. \n(The name shouldn't contain '.' or '-')")
+
 /*--------------------------------------------------------------------
     Description: GraphicSizeCheck strings
  --------------------------------------------------------------------*/

Repository.mk
@@ -1043,6 +1043,7 @@ $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	    favicon \
 	    unoembind \
 	) \
+	svx_xml \
 ))
 
 $(eval $(call gb_Helper_register_packages_for_install,ooo_fonts,\

include/svx/TableStylesParser.hxx
@@ -1,0 +1,155 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#pragma once
+
+#include <svx/svxdllapi.h>
+#include <bitset>
+#include <editeng/borderline.hxx>
+#include <xmloff/xmlexp.hxx>
+#include <xmloff/prstylei.hxx>
+#include <xmloff/table/XMLTableImport.hxx>
+#include <xmloff/xmlimp.hxx>
+#include <xmloff/xmlnamespace.hxx>
+
+class SvxAutoFormat;
+class SvxAutoFormatData;
+class SvxAutoFormatDataField;
+
+struct TableStyle
+{
+    rtl::OUString sParentName;
+    std::unique_ptr<SvxAutoFormatDataField> pDataField;
+};
+
+class SvxTableStylesImport : public SvXMLImport
+{
+private:
+    SvxAutoFormat& mpAutoFormat;
+    std::map<OUString, TableStyle> maCellStyles;
+    XMLTableTemplateMap maTableTemplates;
+    std::map<OUString, std::bitset<6>> maTableStyles;
+
+public:
+    SvxTableStylesImport(const css::uno::Reference<css::uno::XComponentContext>& rContext,
+                         SvxAutoFormat& rAutoFormat);
+    virtual ~SvxTableStylesImport() override;
+
+    // Override from SvXMLImport
+    virtual SvXMLImportContext* CreateFastContext(
+        sal_Int32 nElement,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList>& xAttrList) override;
+
+    void addCellStyle(const OUString& rName, const OUString& rParentName,
+                      std::unique_ptr<SvxAutoFormatDataField> pField);
+    const TableStyle* getCellStyle(const OUString& rName);
+    void addTableTemplate(const OUString& rsStyleName, const XMLTableTemplate& xTableTemplate,
+                          std::bitset<6>& rUseSet);
+    SvxAutoFormat& GetFormat() { return mpAutoFormat; }
+    void finishStyles();
+};
+
+class SvxTableStylesContext : public SvXMLStylesContext
+{
+private:
+    mutable std::unique_ptr<SvXMLImportPropertyMapper> mxTableCellPropMapper;
+    SvxTableStylesImport* mpImport;
+
+public:
+    SvxTableStylesContext(SvxTableStylesImport& rImport);
+
+    virtual SvXMLStyleContext* CreateStyleChildContext(
+        sal_Int32 nElement,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList>& xAttrList) override;
+
+    virtual SvXMLStyleContext* CreateStyleStyleChildContext(
+        XmlStyleFamily nFamily, sal_Int32 nElement,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList>& xAttrList) override;
+
+    virtual SvXMLImportPropertyMapper*
+    GetImportPropertyMapper(XmlStyleFamily nFamily) const override;
+
+    virtual void SAL_CALL endFastElement(sal_Int32 Element) override;
+};
+
+class SvxTableTemplateContext : public SvXMLStyleContext
+{
+private:
+    XMLTableTemplate maTableTemplate;
+    OUString msTemplateName;
+    SvxTableStylesImport* mpImport;
+    std::bitset<6> maUseSet;
+
+protected:
+    virtual void SetAttribute(sal_Int32 nElement, const OUString& rValue) override;
+
+public:
+    SvxTableTemplateContext(SvXMLImport& rImport, SvxTableStylesImport& rSvxImport);
+
+    virtual css::uno::Reference<css::xml::sax::XFastContextHandler> SAL_CALL createFastChildContext(
+        sal_Int32 nElement,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList>& AttrList) override;
+
+    virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
+};
+
+class SvxCellStyleContext : public XMLPropStyleContext
+{
+private:
+    std::unique_ptr<SvxAutoFormatDataField> mpField;
+    SvxTableStylesImport* mpImport;
+
+public:
+    explicit SvxCellStyleContext(SvXMLImport& rImport, SvxTableStylesContext& rStyles,
+                                 SvxTableStylesImport& rSvxImport);
+
+    virtual void Finish(bool bOverwrite) override;
+
+    void setPropertyValue(OUString& rPropName, const css::uno::Any& aValue);
+};
+
+class SVX_DLLPUBLIC SvxTableStylesExport : public SvXMLExport
+{
+private:
+    SvxAutoFormat& mpAutoFormat;
+    std::map<OUString, std::unique_ptr<SvxAutoFormatDataField>> maCellStyles;
+
+    void exportTableTemplate(const SvxAutoFormatData& rData);
+    void exportCellStyle(const SvxAutoFormatDataField& rField, OUString& rStyleName,
+                         OUString& rParentName);
+    void exportCellProperties(const SvxAutoFormatDataField& rField,
+                              const SvxAutoFormatDataField& rParent);
+    void exportParaProperties(const SvxAutoFormatDataField& rField,
+                              const SvxAutoFormatDataField& rParent);
+    void exportTextProperties(const SvxAutoFormatDataField& rField,
+                              const SvxAutoFormatDataField& rParent);
+
+public:
+    SvxTableStylesExport(
+        const css::uno::Reference<css::uno::XComponentContext>& rContext, OUString const& rFileName,
+        const css::uno::Reference<com::sun::star::xml::sax::XDocumentHandler>& xHandler,
+        SvxAutoFormat& rAutoFormat);
+
+    virtual void ExportAutoStyles_() override {}
+    virtual void ExportMasterStyles_() override {}
+    virtual void ExportContent_() override {}
+
+    void ExportStyles();
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit fca86c4703ecc6061bfe1fb77c0e2e4e428a4a30

tdf#172504: Use localized table style names
This is a regression from 4fb31ea85f94c0cffba2282a119811833e584570,
table style names were no longer localized.

Signed-off-by: Karthik <karthik.godha@collabora.com>
Change-Id: I02d0e9cbcce24a6a2747e43c4cf5fe4324e1ad3b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/207825
Tested-by: Jenkins
Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

include/svx/TableAutoFmt.hxx
@@ -202,6 +202,10 @@ public:
     virtual size_t size() const = 0;
     void ResetParent(std::u16string_view rName);
 
+    /** Translate built-in table style names between programmatic name and localized UI name. */
+    static OUString MapProgToUIName(std::u16string_view rProgName);
+    static OUString MapUIToProgName(std::u16string_view rUIName);
+
     static const SfxItemPropertySet& GetTablePropertySet();
 
     bool Load(bool bWriter);

include/svx/strings.hrc
@@ -677,6 +677,7 @@
 #define RID_SVXSTR_COLOR_PINK                               NC_("RID_SVXSTR_COLOR_PINK", "Pink")
 #define RID_SVXSTR_COLOR_TURQUOISE                          NC_("RID_SVXSTR_COLOR_TURQUOISE", "Turquoise")
 /* 16 old AutoFormat Table Styles */
+#define RID_SVXSTR_TBLAFMT_DEFAULT_STYLE                    NC_("RID_SVXSTR_TBLAFMT_DEFAULT_STYLE", "Default Style")
 #define RID_SVXSTR_TBLAFMT_3D                               NC_("RID_SVXSTR_TBLAFMT_3D", "3D")
 #define RID_SVXSTR_TBLAFMT_BLACK1                           NC_("RID_SVXSTR_TBLAFMT_BLACK1", "Black 1")
 #define RID_SVXSTR_TBLAFMT_BLACK2                           NC_("RID_SVXSTR_TBLAFMT_BLACK2", "Black 2")

svx/source/table/tableautofmt.cxx
@@ -42,7 +42,11 @@ common functionality of SwTableAutoFormatTable and ScAutoFormat
 #include <editeng/svxenum.hxx>
 #include <svl/typedwhich.hxx>
 #include <svx/TableStylesParser.hxx>
+#include <svx/dialmgr.hxx>
+#include <svx/strings.hrc>
 #include <tools/stream.hxx>
+#include <map>
+#include <string_view>
 
 using namespace com::sun::star::beans;
 using namespace com::sun::star;
@@ -421,6 +425,38 @@ void SvxAutoFormatData::ResetAutoFormat(SvxAutoFormatData& pOld)
 
 SvxAutoFormat::~SvxAutoFormat() = default;
 
+const std::map<OUString, TranslateId> getTranslatedStyleNames = {
+    { u"Default Style"_ustr, RID_SVXSTR_TBLAFMT_DEFAULT_STYLE },
+    { u"Academic"_ustr, RID_SVXSTR_TBLAFMT_LO6_ACADEMIC },
+    { u"Box List Blue"_ustr, RID_SVXSTR_TBLAFMT_LO6_BOX_LIST_BLUE },
+    { u"Box List Green"_ustr, RID_SVXSTR_TBLAFMT_LO6_BOX_LIST_GREEN },
+    { u"Box List Red"_ustr, RID_SVXSTR_TBLAFMT_LO6_BOX_LIST_RED },
+    { u"Box List Yellow"_ustr, RID_SVXSTR_TBLAFMT_LO6_BOX_LIST_YELLOW },
+    { u"Elegant"_ustr, RID_SVXSTR_TBLAFMT_LO6_ELEGANT },
+    { u"Financial"_ustr, RID_SVXSTR_TBLAFMT_LO6_FINANCIAL },
+    { u"Simple Grid Columns"_ustr, RID_SVXSTR_TBLAFMT_LO6_SIMPLE_GRID_COLUMNS },
+    { u"Simple Grid Rows"_ustr, RID_SVXSTR_TBLAFMT_LO6_SIMPLE_GRID_ROWS },
+    { u"Simple List Shaded"_ustr, RID_SVXSTR_TBLAFMT_LO6_SIMPLE_LIST_SHADED },
+};
+
+OUString SvxAutoFormat::MapProgToUIName(std::u16string_view rProgName)
+{
+    const OUString aProgName(rProgName);
+    auto it = getTranslatedStyleNames.find(aProgName);
+    return it != getTranslatedStyleNames.end() ? SvxResId(it->second) : aProgName;
+}
+
+OUString SvxAutoFormat::MapUIToProgName(std::u16string_view rUIName)
+{
+    const OUString aUIName(rUIName);
+    for (auto const & [ rProgName, rResId ] : getTranslatedStyleNames)
+    {
+        if (aUIName == SvxResId(rResId))
+            return rProgName;
+    }
+    return aUIName;
+}
+
 void SvxAutoFormat::ResetParent(std::u16string_view rName)
 {
     OUString sParent;
@@ -470,6 +506,14 @@ bool SvxAutoFormat::Load(bool bWriter)
         aInputSource.sSystemId = sPath;
 
         xImport->parseStream(aInputSource);
+
+        for (size_t i = 0; i < size(); ++i)
+        {
+            SvxAutoFormatData* pData = GetData(i);
+            pData->SetName(MapProgToUIName(pData->GetName()));
+            if (!pData->GetParent().isEmpty())
+                pData->SetParent(MapProgToUIName(pData->GetParent()));
+        }
         return true;
     }
     catch (const uno::Exception& e)

svx/source/table/tablestylesparser.cxx
@@ -596,15 +596,18 @@ void SvxTableStylesExport::ExportStyles()
         for (size_t i = 0; i < mpAutoFormat.size(); i++)
         {
             const SvxAutoFormatData* pData = mpAutoFormat.GetData(i);
-            const OUString& sTemplateName = pData->GetName();
+            const OUString sTemplateName = SvxAutoFormat::MapUIToProgName(pData->GetName());
+            const OUString sParentProgName
+                = !pData->GetParent().isEmpty() ? SvxAutoFormat::MapUIToProgName(pData->GetParent())
+                                                : OUString();
 
             auto it = CellFieldToIndex.begin();
             while (it != CellFieldToIndex.end())
             {
                 OUString sStyleName = sTemplateName.replaceAll(" ", "-") + "." + it->first;
                 OUString sParentName;
-                if (pData->GetParent().getLength())
-                    sParentName = pData->GetParent().replaceAll(" ", "-") + "." + it->first;
+                if (!sParentProgName.isEmpty())
+                    sParentName = sParentProgName.replaceAll(" ", "-") + "." + it->first;
 
                 exportCellStyle(*pData->GetField(it->second), sStyleName, sParentName);
                 it++;
@@ -623,7 +626,9 @@ void SvxTableStylesExport::ExportStyles()
 
 void SvxTableStylesExport::exportTableTemplate(const SvxAutoFormatData& rData)
 {
-    AddAttribute(XML_NAMESPACE_TABLE, XML_NAME, rData.GetName());
+    const OUString sProgName = SvxAutoFormat::MapUIToProgName(rData.GetName());
+
+    AddAttribute(XML_NAMESPACE_TABLE, XML_NAME, sProgName);
     if (rData.UseFirstRowStyles())
         AddAttribute(XML_NAMESPACE_TABLE, XML_USE_FIRST_ROW_STYLES, "true");
     if (rData.UseLastRowStyles())
@@ -641,8 +646,7 @@ void SvxTableStylesExport::exportTableTemplate(const SvxAutoFormatData& rData)
 
     for (sal_uInt16 i = 0; i < 16; i++)
     {
-        OUString sCellStyleName
-            = rData.GetName().replaceAll(" ", "-") + "." + IndexToCellField.at(i);
+        OUString sCellStyleName = sProgName.replaceAll(" ", "-") + "." + IndexToCellField.at(i);
         AddAttribute(XML_NAMESPACE_TABLE, XML_STYLE_NAME, sCellStyleName);
 
         if (i < 10)

sw/inc/strings.hrc
@@ -234,8 +234,6 @@
 #define STR_COLUMN_VALUESET_ITEM2               NC_("STR_COLUMN_VALUESET_ITEM2", "3 columns with equal size")
 #define STR_COLUMN_VALUESET_ITEM3               NC_("STR_COLUMN_VALUESET_ITEM3", "2 columns with different size (left > right)")
 #define STR_COLUMN_VALUESET_ITEM4               NC_("STR_COLUMN_VALUESET_ITEM4", "2 columns with different size (left < right)")
-// Table styles, Writer internal, others are taken from Svx
-#define STR_TABSTYLE_DEFAULT                    NC_("STR_TABSTYLE_DEFAULT", "Default Style")
 
 #define STR_PARAGRAPHSTYLEFAMILY                NC_("STR_PARAGRAPHSTYLEFAMILY", "Paragraph Styles")
 #define STR_CHARACTERSTYLEFAMILY                NC_("STR_CHARACTERSTYLEFAMILY", "Character Styles")

sw/source/core/doc/DocumentStylePoolManager.cxx
@@ -543,9 +543,8 @@ const TranslateId STR_POOLNUMRULE_NUM_ARY[] =
 // RES_POOL_TABLESTYLE_TYPE in sw/inc/poolfmt.hxx
 const TranslateId STR_TABSTYLE_ARY[] =
 {
-    // XXX MUST be in order, Writer first, then Svx old, then Svx new
-    // 1 Writer resource string
-    STR_TABSTYLE_DEFAULT,
+    // XXX MUST be in order: default style, then Svx old, then Svx new
+    RID_SVXSTR_TBLAFMT_DEFAULT_STYLE,
     // 16 old styles Svx resource strings
     RID_SVXSTR_TBLAFMT_3D,
     RID_SVXSTR_TBLAFMT_BLACK1,
@@ -2847,9 +2846,8 @@ const std::vector<OUString>& SwStyleNameMapper::GetTableStyleUINameArray()
     auto it = s_aTableStyleUINameArray.find(rCurrentLanguage);
     if (it == s_aTableStyleUINameArray.end())
         it = s_aTableStyleUINameArray.emplace(rCurrentLanguage,
-            // 1 Writer resource string (XXX if this ever changes rather use offset math)
             lcl_NewUINameArray(STR_TABSTYLE_ARY, std::ssize(STR_TABSTYLE_ARY),
-                    static_cast<size_t>(std::ssize(STR_TABSTYLE_ARY) - 1))).first;
+                    static_cast<size_t>(std::ssize(STR_TABSTYLE_ARY)))).first;
 
     return it->second;
 }

sw/source/core/doc/tblafmt.cxx
@@ -23,7 +23,7 @@
 #include <doc.hxx>
 #include <tblafmt.hxx>
 #include <cellatr.hxx>
-#include <SwStyleNameMapper.hxx>
+#include <swtypes.hxx>
 #include <hintids.hxx>
 #include <fmtornt.hxx>
 #include <editsh.hxx>

sw/source/core/unocore/unostyle.cxx
@@ -36,7 +36,9 @@
 #include <svl/numformat.hxx>
 #include <svl/zforlist.hxx>
 #include <svl/zformat.hxx>
+#include <svx/dialmgr.hxx>
 #include <svx/pageitem.hxx>
+#include <svx/strings.hrc>
 #include <editeng/colritem.hxx>
 #include <editeng/contouritem.hxx>
 #include <editeng/crossedoutitem.hxx>
@@ -1083,7 +1085,7 @@ void SwXStyleFamily::insertByName(const OUString& rProgName, const uno::Any& rEl
 
         pNewStyle->setName(rProgName); // insertByName sets the element name
         if (pNewStyle->getParentStyle().isEmpty())
-            pNewStyle->setParentStyle(SwResId(STR_TABSTYLE_DEFAULT));
+            pNewStyle->setParentStyle(SvxResId(RID_SVXSTR_TBLAFMT_DEFAULT_STYLE));
 
         m_pDocShell->GetDoc()->GetTableStyles().AddAutoFormat(*pNewStyle->GetTableFormat());
         pNewStyle->SetPhysical();

sw/source/uibase/shells/basesh.cxx
@@ -18,6 +18,7 @@
  */
 
 #include <svx/dialmgr.hxx>
+#include <svx/strings.hrc>
 #include <config_features.h>
 #include <config_fuzzers.h>
 
@@ -3265,7 +3266,7 @@ void SwBaseShell::InsertTable( SfxRequest& _rRequest )
             // Use Default Style if no autoformat is provided
             else
             {
-                aAutoNameIn = SvxResId(STR_TABSTYLE_DEFAULT);
+                aAutoNameIn = SvxResId(RID_SVXSTR_TBLAFMT_DEFAULT_STYLE);
                 pTAFormatIn.reset(new SwTableAutoFormat(rTableTable[0]));
             }
 
@@ -3320,7 +3321,7 @@ void SwBaseShell::InsertTable( SfxRequest& _rRequest )
 
             // Set Default Style name is no autoformat is provided
             if (aAutoNameIn.isEmpty())
-                aAutoNameIn = SvxResId(STR_TABSTYLE_DEFAULT);
+                aAutoNameIn = SvxResId(RID_SVXSTR_TBLAFMT_DEFAULT_STYLE);
 
             InsertTableImpl( rSh, rTempView, UIName(aTableNameIn), nRowsIn, nColsIn, aInsTableOptsIn, TableStyleName(aAutoNameIn), pTAFormatIn );
 